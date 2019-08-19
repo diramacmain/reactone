806737ea7196e879b19adb6b49a3b2fa92328d58
Hlanga <henry.bani.hb@gmail.com>
2019-08-19 15:24:53 +0200

    clean up
    cleared up some code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Header.js'
+import Footer from './Footer.js'
 function App(){
   return(
+    <div className="appBody">
     <Header/>
+    <Footer/>
+    </div>
   )
 }
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './App.css';
+
+function Footer(){
+    return(
+        <div>Footer</div>
+    )
+}export default Footer;
